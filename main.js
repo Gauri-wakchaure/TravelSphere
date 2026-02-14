bc41f62d6d6594b06dd813e78061efcb5f00b718
Gauri-wakchaure <gaureewakchaure07@gmail.com>
2026-02-14 17:11:55 +0530

    Making some changes in existing code and made package.html file

--- main.js
+++ main.js
@@ -39,3 +39,29 @@ $(document).ready(function(){
     AOS.init()
 
 });
+
+function openForm() {
+    document.getElementById("bookingForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("bookingForm").style.display = "none";
+}
+
+function submitForm(e) {
+    e.preventDefault();
+
+    let name = document.getElementById("name").value;
+    let phone = document.getElementById("phone").value;
+    let email = document.getElementById("email").value;
+
+    alert(
+        "Booking Confirmed!\n\n" +
+        "Name: " + name + "\n" +
+        "Phone: " + phone + "\n" +
+        "Email: " + email
+    );
+
+    closeForm();
+}
+
